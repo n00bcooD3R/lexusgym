b1daae09de7c4502d2a866a8694927eb9c140f95
n00bcooD3R <n00bcooD3R@users.noreply.github.com>
2026-06-08 12:22:34 +0530

    Remove deprecated root-level Next.js routes and configurations

--- lib/upi.ts
+++
@@ -1,4 +0,0 @@
-export function generateUPIQR(amount: number, upiId: string = "lexusfitness@upi") {
-  const upiLink = `upi://pay?pa=${upiId}&pn=Lexus%20Fitness%20Group&am=${amount}&cu=INR`;
-  return upiLink;
-}
